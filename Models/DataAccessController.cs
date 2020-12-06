e6655ccdd21828af8bc6dcdbe3a1c96c86f53d4e
TomReidNZ <treid.nz@gmail.com>
2020-12-07 11:13:16 +1300

    init app

---
+++ Models/DataAccessController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentUsersApp.Models
+{
+    public class DataAccessController
+    {
+        // TODO: Add your connection string in the following statements
+        string connectionString = "<Azure Database for PostgreSQL Connection String>";
+
+        // Retrieve all details of courses and their modules    
+        public IEnumerable<Users> GetAllUsers()
+        {
+            List<Users> userList = new List<Users>();
+
+            // TODO: Connect to the database
+            //using ()
+
+            {
+                Console.Out.WriteLine("Opening connection");
+
+                // TODO: Specify the SQL query to run
+
+                // TODO: Execute the query
+
+                // TODO: Read the data a row at a time
+            
+                // TODO: Close the database connection
+            }
+            return userList;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PaymentUsersApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PaymentUsersApp.Pages
+{
+    public class UsersModel : PageModel
+    {
+        // Create a DataAccessController object
+        DataAccessController dac = new DataAccessController();
+
+        // Create a collection for holding CoursesAndModules object
+        public List<Users> Users;
+
+        public void OnGet()
+        {
+            // Retrieve the data using the DataAccessController object and populate the CoursesAndModules object
+            Users = dac.GetAllUsers().ToList();
+        }
+    }
+}
